8a731aba959c97529be8a716a7e56123feb36353
Jhan Bayer <jhanbayer@gmail.com>
2022-05-18 13:51:10 -0400

    fix: fix import context to the hook

--- src/hooks/useLetras.jsx
+++ src/hooks/useLetras.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import LetrasContext from "./context/LetrasProvider";
+import LetrasContext from "../context/LetrasProvider";
 
 const useLetras = () => {
   return useContext(LetrasContext);
